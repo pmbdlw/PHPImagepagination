a18dda8ceed9c325062d4f323acf267ad606b52d
刘义德 <pmbdlw@gmail.com>
2017-05-23 01:39:19 +0800

    Version 1.0 is finished.

--- PHPImagepagination.php
+++ PHPImagepagination.php
@@ -1,5 +1,6 @@
 <?php
 namespace PIP;
+
 use Exception;
 
 class PHPImagepagination
@@ -9,42 +10,98 @@ class PHPImagepagination
     private $totalPages;
     private $recordCount;
     private $filePath;
+    private $allFiles;
     
-    function __construct($filePath,$pageIndex = 1, $pageSize = 10, $recordCount = 0)
+    function __construct($filePath, $pageIndex = 1, $pageSize = 10)
     {
-        if(empty($filePath)){
+        if (empty($filePath)) {
             throw new Exception("the argument \"filePath\" is empty.");
         }
         $this->filePath = $filePath;
         $this->pageIndex = $pageIndex;
         $this->pageSize = $pageSize;
-        $this->recordCount = $recordCount;
-        $this->totalPages = $recordCount / $pageSize;
+        $this->InitData();
     }
 
-    public function GetPageIndex(){
+    private function InitData(){
+        $this->GetFilesByPath();
+        $this->recordCount = count($this->allFiles);
+        $this->totalPages = ceil($this->recordCount / $this->pageSize);
+        if($this->pageIndex > $this->totalPages){
+            $this->pageIndex = $this->totalPages;
+        }
+
+        if($this->pageIndex < 1){
+            $this->pageIndex = 1;
+        }
+    }
+
+    public function GetPageIndex()
+    {
         return  $this->pageIndex;
     }
 
-    public  function GetPageSize(){
+    public function GetPageSize()
+    {
         return $this->pageSize;
     }
 
-    public  function GetFilePath(){
+    public function GetFilePath()
+    {
         return $this->filePath;
     }
 
-    public function GetFilesByPath(){
-        if(empty($this->filePath)){
+    public function GetRecordCount()
+    {
+        return $this->recordCount;
+    }
+
+    public function GetTotalPages()
+    {
+        return $this->totalPages;
+    }
+
+    public function GetFiles()
+    {
+        $currentFiles = array();
+        $page = $this->pageIndex - 1;
+        $start = $this->pageSize*$page;
+        $end = ($page+1)*$this->pageSize;
+        if($this->IsLastPage() && ($this->recordCount-1 < $end)){
+            $end = $this->recordCount - 1;
+        }
+        for ($i=$start; $i<$end; $i++) {
+            array_push($currentFiles, $this->allFiles[$i]);
+        }
+        return $currentFiles;
+    }
+
+    private function IsFirstPage(){
+        if($this->pageIndex == 1){
+            return true;
+        }
+        return false;
+    }
+
+    private function IsLastPage(){
+        if($this->pageIndex == $this->totalPages){
+            return  true;
+        }
+        return false;
+    }
+
+    private function GetFilesByPath()
+    {
+        if (empty($this->filePath)) {
             return  array();
         }
-        if(!is_dir($this->filePath)){
-            throw new Exception("Your filePath is not a directory.", 1);            
+        if (!is_dir($this->filePath)) {
+            throw new Exception("Your filePath is not a directory.", 1);
         }
         $files = scandir($this->filePath);
-        if(empty($files)){
-            $files = array_values(array_diff($files,array('.','..','.DS_Store')));
+        if (empty($files)) {
+            throw new Exception("The file folder is empty.", 1);
         }
-        return $files;
+        $this->allFiles = array_values(array_diff($files, array('.','..','.DS_Store')));
     }
 }
